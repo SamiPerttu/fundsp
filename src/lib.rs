70b8749a56e7e5a8b305397365aff9e266fbaf4b
Sami Perttu <Sami.Perttu@gmail.com>
2022-01-24 21:24:04 +0200

    Remove unused code.

--- src/lib.rs
+++ src/lib.rs
@@ -211,45 +211,6 @@ macro_rules! impl_real {
 }
 impl_real! { f32, f64 }
 
-pub trait AsPrimitive<T: Copy>: Copy {
-    /// Convert a value using the as operator.
-    fn as_(self) -> T;
-}
-
-macro_rules! impl_as_primitive {
-    (@ $T: ty => $(#[$cfg:meta])* impl $U: ty ) => {
-        $(#[$cfg])*
-        impl AsPrimitive<$U> for $T {
-            #[inline] fn as_(self) -> $U { self as $U }
-        }
-    };
-    (@ $T: ty => { $( $U: ty ),* } ) => {$(
-        impl_as_primitive!(@ $T => impl $U);
-    )*};
-    ($T: ty => { $( $U: ty ),* } ) => {
-        impl_as_primitive!(@ $T => { $( $U ),* });
-        impl_as_primitive!(@ $T => { u8, u16, u32, u64, u128, usize });
-        impl_as_primitive!(@ $T => { i8, i16, i32, i64, i128, isize });
-    };
-}
-
-impl_as_primitive!(u8 => { char, f32, f64 });
-impl_as_primitive!(i8 => { f32, f64 });
-impl_as_primitive!(u16 => { f32, f64 });
-impl_as_primitive!(i16 => { f32, f64 });
-impl_as_primitive!(u32 => { f32, f64 });
-impl_as_primitive!(i32 => { f32, f64 });
-impl_as_primitive!(u64 => { f32, f64 });
-impl_as_primitive!(i64 => { f32, f64 });
-impl_as_primitive!(u128 => { f32, f64 });
-impl_as_primitive!(i128 => { f32, f64 });
-impl_as_primitive!(usize => { f32, f64 });
-impl_as_primitive!(isize => { f32, f64 });
-impl_as_primitive!(f32 => { f32, f64 });
-impl_as_primitive!(f64 => { f32, f64 });
-impl_as_primitive!(char => { char });
-impl_as_primitive!(bool => {});
-
 pub mod audionode;
 pub mod audiounit;
 pub mod buffer;
