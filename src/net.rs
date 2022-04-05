178762d887275f8c7ddf2d4eda4c555766031127
Sami Perttu <Sami.Perttu@gmail.com>
2022-04-05 16:36:08 +0300

    Net32.

--- src/net.rs
+++ src/net.rs
@@ -5,7 +5,7 @@ use super::audiounit::*;
 use super::buffer::*;
 use super::signal::*;
 use super::*;
-//use duplicate::duplicate_item;
+use duplicate::duplicate_item;
 
 pub type NodeIndex = usize;
 pub type PortIndex = usize;
@@ -32,28 +32,38 @@ pub fn edge(source: Port, target: Port) -> Edge {
     Edge { source, target }
 }
 
+#[duplicate_item(
+    f48       Vertex48       AudioUnit48;
+    [ f64 ]   [ Vertex64 ]   [ AudioUnit64 ];
+    [ f32 ]   [ Vertex32 ]   [ AudioUnit32 ];
+)]
 /// Individual AudioUnits are vertices in the graph.
-pub struct Vertex64 {
+pub struct Vertex48 {
     /// The unit.
-    pub unit: Box<dyn AudioUnit64>,
+    pub unit: Box<dyn AudioUnit48>,
     /// Edges connecting into this vertex. The length indicates the number of inputs.
     pub source: Vec<Edge>,
     /// Input buffers. The length indicates the number of inputs.
-    pub input: Buffer<f64>,
+    pub input: Buffer<f48>,
     /// Output buffers. The length indicates the number of outputs.
-    pub output: Buffer<f64>,
+    pub output: Buffer<f48>,
     /// Input for tick iteration. The length indicates the number of inputs.
-    pub tick_input: Vec<f64>,
+    pub tick_input: Vec<f48>,
     /// Output for tick iteration. The length indicates the number of outputs.
-    pub tick_output: Vec<f64>,
+    pub tick_output: Vec<f48>,
     /// Index or ID of this unit. This equals unit index in graph.
     pub id: NodeIndex,
 }
 
-impl Vertex64 {
+#[duplicate_item(
+    f48       Vertex48       AudioUnit48;
+    [ f64 ]   [ Vertex64 ]   [ AudioUnit64 ];
+    [ f32 ]   [ Vertex32 ]   [ AudioUnit32 ];
+)]
+impl Vertex48 {
     pub fn new(id: NodeIndex, inputs: usize, outputs: usize) -> Self {
         Self {
-            unit: Box::new(super::hacker::pass()),
+            unit: Box::new(super::prelude::pass()),
             source: vec![],
             input: Buffer::with_size(inputs),
             output: Buffer::with_size(outputs),
@@ -72,21 +82,35 @@ impl Vertex64 {
     }
 }
 
-pub struct Net64 {
+#[duplicate_item(
+    f48       Net48       Vertex48       AudioUnit48;
+    [ f64 ]   [ Net64 ]   [ Vertex64 ]   [ AudioUnit64 ];
+    [ f32 ]   [ Net32 ]   [ Vertex32 ]   [ AudioUnit32 ];
+)]
+/// Network unit. It can contain other units and maintain connections between them.
+/// Outputs of the network are sourced from user specified unit outputs or global inputs.
+pub struct Net48 {
     /// Global input buffers.
-    input: Buffer<f64>,
+    input: Buffer<f48>,
     /// Global output buffers.
-    output: Buffer<f64>,
+    output: Buffer<f48>,
     /// Sources of global outputs.
     output_edge: Vec<Edge>,
     /// Vertices of the graph.
-    vertex: Vec<Vertex64>,
+    vertex: Vec<Vertex48>,
     /// Ordering of vertex evaluation.
     order: Vec<NodeIndex>,
     ordered: bool,
 }
 
-impl Net64 {
+#[duplicate_item(
+    f48       Net48       Vertex48       AudioUnit48;
+    [ f64 ]   [ Net64 ]   [ Vertex64 ]   [ AudioUnit64 ];
+    [ f32 ]   [ Net32 ]   [ Vertex32 ]   [ AudioUnit32 ];
+)]
+impl Net48 {
+    /// Create new network with the given number of inputs and outputs.
+    /// The number of inputs and outputs is fixed after construction.
     pub fn new(inputs: usize, outputs: usize) -> Self {
         let mut net = Self {
             input: Buffer::with_size(inputs),
@@ -103,7 +127,7 @@ impl Net64 {
         net
     }
 
-    pub fn determine_order(&mut self) {
+    fn determine_order(&mut self) {
         self.ordered = true;
         let mut order = Vec::new();
         self.determine_order_in(&mut order);
@@ -111,7 +135,7 @@ impl Net64 {
         std::mem::swap(&mut order, &mut self.order);
     }
 
-    pub fn determine_order_in(&self, order: &mut Vec<NodeIndex>) {
+    fn determine_order_in(&self, order: &mut Vec<NodeIndex>) {
         let mut vertices_left = self.vertex.len();
         let mut vertex_left = vec![true; self.vertex.len()];
         // Note about contents of the edge vector.
@@ -171,11 +195,13 @@ impl Net64 {
         }
     }
 
-    pub fn add(&mut self, unit: Box<dyn AudioUnit64>) -> NodeIndex {
+    /// Add a new unit to the network. Return its ID handle.
+    /// ID handles are always consecutive numbers starting from zero.
+    pub fn add(&mut self, unit: Box<dyn AudioUnit48>) -> NodeIndex {
         let id = self.vertex.len();
         let inputs = unit.inputs();
         let outputs = unit.outputs();
-        let mut vertex = Vertex64 {
+        let mut vertex = Vertex48 {
             unit,
             source: vec![],
             input: Buffer::with_size(inputs),
@@ -194,6 +220,8 @@ impl Net64 {
         id
     }
 
+    /// Connect the given output (`source`, `source_port`)
+    /// to the given input (`target`, `target_port`).
     pub fn connect(
         &mut self,
         source: NodeIndex,
@@ -208,6 +236,7 @@ impl Net64 {
         self.ordered = false;
     }
 
+    /// Connect the node input (`target`, `target_port`) to the global input `global_input`.
     pub fn connect_input(
         &mut self,
         global_input: PortIndex,
@@ -219,6 +248,7 @@ impl Net64 {
         self.ordered = false;
     }
 
+    /// Pipe global input to node `target`.
     pub fn pipe_input(&mut self, target: NodeIndex) {
         assert!(self.vertex[target].inputs() == self.inputs());
         for i in 0..self.inputs() {
@@ -227,6 +257,7 @@ impl Net64 {
         self.ordered = false;
     }
 
+    /// Connect node input (`source`, `source_port`) to global output `global_output`.
     pub fn connect_output(
         &mut self,
         source: NodeIndex,
@@ -240,6 +271,8 @@ impl Net64 {
         self.ordered = false;
     }
 
+    /// Pipe node outputs to global outputs.
+    /// The number of outputs and number of global outputs must match.
     pub fn pipe_output(&mut self, source: NodeIndex) {
         assert!(self.vertex[source].outputs() == self.outputs());
         for i in 0..self.outputs() {
@@ -248,6 +281,7 @@ impl Net64 {
         self.ordered = false;
     }
 
+    /// Add an arbitrary edge to the network.
     pub fn join(&mut self, edge: Edge) {
         match edge.target {
             Port::Global(global_output) => self.output_edge[global_output] = edge,
@@ -257,6 +291,8 @@ impl Net64 {
         self.ordered = false;
     }
 
+    /// Connect `source` to `target`.
+    /// The number of outputs in `source` and number of inputs in `target` must match.
     pub fn pipe(&mut self, source: NodeIndex, target: NodeIndex) {
         assert!(self.vertex[source].outputs() == self.vertex[target].inputs());
         for i in 0..self.vertex[target].inputs() {
@@ -265,7 +301,9 @@ impl Net64 {
         self.ordered = false;
     }
 
-    pub fn chain(&mut self, unit: Box<dyn AudioUnit64>) {
+    /// Assuming this network is a chain of processing units ordered by increasing node ID,
+    /// add a new unit to the chain. The global outputs will be assigned to the outputs of the unit.
+    pub fn chain(&mut self, unit: Box<dyn AudioUnit48>) {
         assert!(unit.inputs() == unit.outputs() && self.outputs() == unit.outputs());
         let id = self.add(unit);
         self.pipe_output(id);
@@ -277,7 +315,12 @@ impl Net64 {
     }
 }
 
-impl AudioUnit64 for Net64 {
+#[duplicate_item(
+    f48       Net48       Vertex48       AudioUnit48;
+    [ f64 ]   [ Net64 ]   [ Vertex64 ]   [ AudioUnit64 ];
+    [ f32 ]   [ Net32 ]   [ Vertex32 ]   [ AudioUnit32 ];
+)]
+impl AudioUnit48 for Net48 {
     fn inputs(&self) -> usize {
         self.input.buffers()
     }
@@ -292,13 +335,13 @@ impl AudioUnit64 for Net64 {
         }
     }
 
-    fn tick(&mut self, input: &[f64], output: &mut [f64]) {
+    fn tick(&mut self, input: &[f48], output: &mut [f48]) {
         if !self.ordered {
             self.determine_order();
         }
         // Iterate units in network order.
         for node_index in self.order.iter() {
-            let mut vertex = Vertex64::new(*node_index, 0, 0);
+            let mut vertex = Vertex48::new(*node_index, 0, 0);
 
             std::mem::swap(&mut vertex, &mut self.vertex[*node_index]);
             for channel in 0..vertex.inputs() {
@@ -326,13 +369,13 @@ impl AudioUnit64 for Net64 {
         }
     }
 
-    fn process(&mut self, size: usize, input: &[&[f64]], output: &mut [&mut [f64]]) {
+    fn process(&mut self, size: usize, input: &[&[f48]], output: &mut [&mut [f48]]) {
         if !self.ordered {
             self.determine_order();
         }
         // Iterate units in network order.
         for node_index in self.order.iter() {
-            let mut vertex = Vertex64::new(*node_index, 0, 0);
+            let mut vertex = Vertex48::new(*node_index, 0, 0);
 
             std::mem::swap(&mut vertex, &mut self.vertex[*node_index]);
             for channel in 0..vertex.inputs() {
