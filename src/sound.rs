476b252bde2955c5c4c2731aaf02f8307ae95ae1
Sami Perttu <Sami.Perttu@gmail.com>
2022-04-06 16:24:11 +0300

    Edit.

--- src/sound.rs
+++ src/sound.rs
@@ -8,9 +8,10 @@ pub fn risset_glissando(up: bool) -> An<impl AudioNode<Sample = f64, Inputs = U0
     stack::<U40, _, _>(|i| {
         lfo(move |t| {
             let (f0, f1) = if up { (20.0, 20480.0) } else { (20480.0, 20.0) };
+            let phase = (t * 0.1 + i as f64 * 10.0 / 40.0) % 10.0 / 10.0;
             let f = lerp(-1.0, 1.0, rnd(i))
-                + xerp(f0, f1, (t * 0.1 + i as f64 * 10.0 / 40.0) % 10.0 / 10.0);
-            let a = smooth3(sin_hz(0.05, (t * 0.1 + i as f64 * 0.5) % 10.0));
+                + xerp(f0, f1, phase);
+            let a = smooth3(sin_hz(0.5, phase));
             (a, f)
         }) >> pass() * sine()
     }) >> multijoin::<U2, U20>()
