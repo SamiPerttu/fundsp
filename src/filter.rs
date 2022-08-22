c43636bead0a32fa3e77a61c3f542f5b891756ff
Sami Perttu <Sami.Perttu@gmail.com>
2022-08-22 19:05:16 +0300

    Improve precision.

--- src/filter.rs
+++ src/filter.rs
@@ -441,12 +441,12 @@ fn logistic<T: Real>(x: T) -> T {
     T::one() / (T::one() + exp(T::zero() - x))
 }
 
-fn halfway_coeff<F: Real>(samples: F) -> F {
+fn halfway_coeff(samples: f64) -> f64 {
     // This approximation is accurate to 0.5% when 1 <= response_samples <= 500_000.
-    let r0 = log(max(F::one(), samples)) - F::from_f64(0.861624594696583);
+    let r0 = log(max(1.0, samples)) - 0.861624594696583;
     let r1 = logistic(r0);
-    let r2 = r1 * F::from_f64(1.13228543863477) - F::from_f64(0.1322853859);
-    min(F::one(), r2)
+    let r2 = r1 * 1.13228543863477 - 0.1322853859;
+    1.0 - min(0.9999999, r2)
 }
 
 /// Smoothing filter with adjustable edge response time.
@@ -484,7 +484,7 @@ impl<T: Float, F: Real> Follow<T, F> {
     /// Set response time in seconds.
     pub fn set_response_time(&mut self, response_time: F) {
         self.response_time = response_time;
-        self.coeff = halfway_coeff(response_time * self.sample_rate);
+        self.coeff = F::from_f64(halfway_coeff((response_time * self.sample_rate).to_f64()));
     }
 
     /// Current response.
@@ -523,16 +523,16 @@ impl<T: Float, F: Real> AudioNode for Follow<T, F> {
     ) -> Frame<Self::Sample, Self::Outputs> {
         // Three 1-pole filters in series.
         let rcoeff = F::one() - self.coeff;
-        self.v1 = rcoeff * convert(input[0]) + self.coeff * self.v1;
-        self.v2 = rcoeff * self.v1 + self.coeff * self.v2;
-        self.v3 = rcoeff * self.v2 + self.coeff * self.v3;
+        self.v1 = self.coeff * convert(input[0]) + rcoeff * self.v1;
+        self.v2 = self.coeff * self.v1 + rcoeff * self.v2;
+        self.v3 = self.coeff * self.v2 + rcoeff * self.v3;
         [convert(self.v3)].into()
     }
 
     fn route(&self, input: &SignalFrame, frequency: f64) -> SignalFrame {
         let mut output = new_signal_frame(self.outputs());
         output[0] = input[0].filter(0.0, |r| {
-            let c = self.coeff.to_f64();
+            let c = 1.0 - self.coeff.to_f64();
             let f = frequency * TAU / self.sample_rate.to_f64();
             let z1 = Complex64::from_polar(1.0, -f);
             let pole = (1.0 - c) / (1.0 - c * z1);
@@ -583,8 +583,12 @@ impl<T: Float, F: Real, S: ScalarOrPair<Sample = F>> AFollow<T, F, S> {
     /// Set attack/release time in seconds.
     pub fn set_time(&mut self, time: S) {
         self.time = time;
-        self.acoeff = halfway_coeff(self.attack_time() * self.sample_rate);
-        self.rcoeff = halfway_coeff(self.release_time() * self.sample_rate);
+        self.acoeff = F::from_f64(halfway_coeff(
+            (self.attack_time() * self.sample_rate).to_f64(),
+        ));
+        self.rcoeff = F::from_f64(halfway_coeff(
+            (self.release_time() * self.sample_rate).to_f64(),
+        ));
     }
 
     /// Current response.
@@ -623,12 +627,14 @@ impl<T: Float, F: Real, S: ScalarOrPair<Sample = F>> AudioNode for AFollow<T, F,
         input: &Frame<Self::Sample, Self::Inputs>,
     ) -> Frame<Self::Sample, Self::Outputs> {
         // Three 1-pole filters in series.
-        let afactor = F::one() - self.acoeff;
-        let rfactor = F::one() - self.rcoeff;
         let v0: F = convert(input[0]);
-        self.v1 = self.time.filter_pole(v0, self.v1, afactor, rfactor);
-        self.v2 = self.time.filter_pole(self.v1, self.v2, afactor, rfactor);
-        self.v3 = self.time.filter_pole(self.v2, self.v3, afactor, rfactor);
+        self.v1 = self.time.filter_pole(v0, self.v1, self.acoeff, self.rcoeff);
+        self.v2 = self
+            .time
+            .filter_pole(self.v1, self.v2, self.acoeff, self.rcoeff);
+        self.v3 = self
+            .time
+            .filter_pole(self.v2, self.v3, self.acoeff, self.rcoeff);
         [convert(self.v3)].into()
     }
 
@@ -637,7 +643,7 @@ impl<T: Float, F: Real, S: ScalarOrPair<Sample = F>> AudioNode for AFollow<T, F,
         // The frequency response exists only in symmetric mode, as the asymmetric mode is nonlinear.
         if self.acoeff == self.rcoeff {
             output[0] = input[0].filter(0.0, |r| {
-                let c = self.acoeff.to_f64();
+                let c = 1.0 - self.acoeff.to_f64();
                 let f = frequency * TAU / self.sample_rate.to_f64();
                 let z1 = Complex64::from_polar(1.0, -f);
                 let pole = (1.0 - c) / (1.0 - c * z1);
